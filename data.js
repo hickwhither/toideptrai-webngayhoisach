8238ae030982a4b6286cefe892612cc4e00ef775
hickwhither <92677288+hickwhither@users.noreply.github.com>
2026-04-15 21:15:59 +0700

    up lan 2

--- data.js
+++ data.js
@@ -2,6 +2,14 @@ export const books = {
     "Kỹ năng sống": [
         ["Đắc Nhâm Tâm","Dale Carnegie"],
         ["Dám nghĩ lớn", "David J. Schwartz"],
-        ["Đọc vị bất kỳ ai", "David J. Lieberman"]
-    ]
+        ["Đọc vị bất kỳ ai", "David J. Lieberman"],
+        ["Tuổi trẻ đáng giá bao nhiêu", "Rosie Nguyễn"],
+        ["Tư duy tích cực tạo thành công", "Napoleon Hill & W. Clement Stone"]
+        ["Ngày xưa có một con bò", "Camilo Cruz"],
+        ["Trên đường băng", "Tony Buổi Sáng"],
+        ["Sống và suy ngẫm", "V.N. Kakar"],
+    ],
+    "Tiểu thuyết": [
+        ["Nhà giả kim", "Paulo Coelho"],
+    ],
 }
